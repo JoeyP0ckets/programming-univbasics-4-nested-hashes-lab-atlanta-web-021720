0cb7913a89a93bedc36744634d3b74e3c6703c1f
Joseph Patterson <thinkitmakeituseit@gmail.com>
2020-02-14 02:50:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,8 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
-
+                                 programmer_hash[:dennis_ritchie][:languages]dennis_ritchies_new_data
 
 #All you
 programmer_hash[:dennis_ritchie][:languages] = dennis_ritchies_new_data
